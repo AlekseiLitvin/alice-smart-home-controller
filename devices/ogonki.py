396c7c93118261aa04770f6fbbb3feeb0b24bff9
AlekseiLitvin <aliaksei_litvin@epam.com>
2020-08-15 20:13:43 +0300

    bigfix

--- devices/ogonki.py
+++ devices/ogonki.py
@@ -7,7 +7,7 @@ plug = miio.ChuangmiPlug(ip=os.environ['OGONKI_IP'], token=os.environ['OGONKI_TO
 
 def query(capability_type, instance):
     if capability_type == "devices.capabilities.on_off":
-        return plug.status().is_on
+        return not plug.status().is_on
 
 
 def action(capability_type, instance, value, relative):
