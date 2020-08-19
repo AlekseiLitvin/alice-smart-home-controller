7f0c063602877cbf0bc156942c53eb08678ce55d
AlekseiLitvin <aliaksei_litvin@epam.com>
2020-08-19 23:14:21 +0300

    discover fix

--- devices/ogonki.py
+++ devices/ogonki.py
@@ -1,19 +1,32 @@
 import miio
 import os
+import logging
+
+from miio import DeviceException
 
 
-plug = miio.ChuangmiPlug(ip=os.environ['OGONKI_IP'], token=os.environ['OGONKI_TOKEN'])
+max_retries = 5
+retries_count = 0
 
 
 def query(capability_type, instance):
+    plug = miio.ChuangmiPlug(ip=os.environ['OGONKI_IP'], token=os.environ['OGONKI_TOKEN'])
     if capability_type == "devices.capabilities.on_off":
         return not plug.status().is_on
 
 
 def action(capability_type, instance, value, relative):
+    plug = miio.ChuangmiPlug(ip=os.environ['OGONKI_IP'], token=os.environ['OGONKI_TOKEN'])
+    global retries_count
     if capability_type == "devices.capabilities.on_off":
-        if value:
-            plug.on()
-        else:
-            plug.off()
-        return "DONE"
+        try:
+            if value:
+                plug.on()
+            else:
+                plug.off()
+            retries_count += 1
+            return "DONE"
+        except DeviceException:
+            logging.info("Device not found, trying again")
+            if retries_count < max_retries:
+                action(capability_type, instance, value, relative)
