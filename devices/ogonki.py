840b00b623fd6a42b92ef68fdf372cdb90a3587b
AlekseiLitvin <aliaksei_litvin@epam.com>
2020-08-19 23:35:52 +0300

    discover fix

--- devices/ogonki.py
+++ devices/ogonki.py
@@ -2,15 +2,14 @@ import miio
 import os
 
 
-plug = miio.ChuangmiPlug(ip=os.environ['OGONKI_IP'], token=os.environ['OGONKI_TOKEN'])
-
-
 def query(capability_type, instance):
+    plug = miio.ChuangmiPlug(ip=os.environ['OGONKI_IP'], token=os.environ['OGONKI_TOKEN'])
     if capability_type == "devices.capabilities.on_off":
         return not plug.status().is_on
 
 
 def action(capability_type, instance, value, relative):
+    plug = miio.ChuangmiPlug(ip=os.environ['OGONKI_IP'], token=os.environ['OGONKI_TOKEN'])
     if capability_type == "devices.capabilities.on_off":
         if value:
             plug.on()
