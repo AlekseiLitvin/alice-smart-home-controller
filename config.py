358cc7ada431f539dd86f5cc37f91461aa681e7c
AlekseiLitvin <aliaksei_litvin@epam.com>
2020-08-20 22:35:02 +0300

    Bugfix

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DEVICES_DIRECTORY = "devices"
 
 
 # Uncomment to enable logging
-LOG_FILE = "/home/pi/Desktop/logs/alice.log"
+# LOG_FILE = "/home/pi/Desktop/logs/alice.log"
 LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = "%(asctime)s %(message)s"
 LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
--- devices/ogonki.py
+++ devices/ogonki.py
@@ -2,19 +2,20 @@ import os
 from miio import ChuangmiPlug
 import logging
 
-
-plug = ChuangmiPlug(ip=os.environ['OGONKI_IP'], token=os.environ['OGONKI_TOKEN'], debug=True)
+ip = os.environ['OGONKI_IP']
+token = os.environ['OGONKI_TOKEN']
+plug = ChuangmiPlug(ip=ip, token=token, debug=True)
 logger = logging.getLogger()
 
 
 def query(capability_type, instance):
     logger.info("Query device, status {}, capability_type {}".format(plug.status().is_on, capability_type))
     if capability_type == "devices.capabilities.on_off":
-        return not plug.status().is_on
+        return plug.status().is_on
 
 
 def action(capability_type, instance, value, relative):
-    logger.info("Action, value {}".format(value))
+    logger.info(f"Action, value {value}")
     if capability_type == "devices.capabilities.on_off":
         if value:
             plug.on()
