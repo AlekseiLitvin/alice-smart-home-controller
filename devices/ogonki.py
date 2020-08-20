6371576467b2d231ecb61bcfa37c68dec909763e
AlekseiLitvin <aliaksei_litvin@epam.com>
2020-08-20 23:27:59 +0300

    Bugfix

--- devices/ogonki.py
+++ devices/ogonki.py
@@ -5,20 +5,15 @@ import logging
 ip = os.environ['OGONKI_IP']
 token = os.environ['OGONKI_TOKEN']
 plug = ChuangmiPlug(ip=ip, token=token, debug=True)
-isOn = plug.status().is_on
 logger = logging.getLogger()
 
 
 def query(capability_type, instance):
-    global isOn
-    logger.info("Query device, status {}, capability_type {}".format(plug.status().is_on, capability_type))
     if capability_type == "devices.capabilities.on_off":
-        isOn = not isOn
-        return
+        return plug.status().power
 
 
 def action(capability_type, instance, value, relative):
-    logger.info(f"Action, value {value}")
     if capability_type == "devices.capabilities.on_off":
         if value:
             plug.on()
